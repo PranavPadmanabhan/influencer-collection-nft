63a6fb9d550d3e5fb1c0188b65d73cfd79dfcd04
PranavPadmanabhan <PranavPadmanabhan77@gmail.com>
2022-07-19 20:59:49 +0530

    mint page desktop and mobile  view finished

--- src/pages/mint/index.tsx
+++ src/pages/mint/index.tsx
@@ -25,7 +25,7 @@ const Mint:NextPage = () => {
             <h1 className="sixed top-[17%] text-white font-light text-[2rem] sm:mb-3">MINT NOW</h1>
             <div className="z-[100] w-[100%] h-auto min-h-[60%] overflow-y-scroll flex flex-col items-center justify-start sm:grid sm:grid-cols-2 sm:place-content-start sm:place-items-center sm:gap-y-3 md:grid md:grid-cols-3 md:place-content-start md:place-items-center md:gap-y-5 snap-y snap-mandatory scroll-pt-9 pb-[10vh]">
                 <NFTCard url={data[0]} price={0.88} name={'Elon Musk'}/>
-                <NFTCard url={data[1]} price={0.88} name={'Pranav Padmanabhan'}/>
+                <NFTCard url={data[1]} price={0.88} name={'Elon Musk'}/>
                 <NFTCard url={data[2]} price={0.88} name={'Elon Musk'}/>
                 <NFTCard url={data[3]} price={0.88} name={'Elon Musk'}/>
                 <NFTCard url={data[0]} price={0.88} name={'Elon Musk'}/>
